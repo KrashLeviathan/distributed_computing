503b607288187f1430a0cbbaf165b9a6dcd9ec04
bcannon <bcannon@iastate.edu>
2016-10-11 11:43:07 -0500

    Added more to worker_client

--- worker_client.py
+++ worker_client.py
@@ -4,47 +4,12 @@ import pickle
 import socket
 import sys
 import zipfile
+import os.path
 
 HOST, PORT = "localhost", 9999
 data = " ".join(sys.argv[1:])
-zfile_path = "./hello_w.zip"
-pklfile_path = "./*.pkl"
-
-# Create a socket (SOCK_STREAM means a TCP socket)                                       
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-try:
-    # Connect to server and send data                                                    
-    sock.connect((HOST, PORT))
-    sock.sendall("Ready to do work")
-
-    # Receive data from the server and shut down                                         
-    received = sock.recv(1024)
-	
-	# TODO Set vars to received information
-	# Save file to zfile_path with data received from sock
-	
-	# Unzip calculation.zip
-	zip_ref = zipfile.ZipFile(zfile_path, 'r')
-	zip_ref.extractall("./")
-	zip_ref.close()
-	
-	# TODO Create the file at pklfile_path from sock
-	# write_data_to_file(pklfile_path, received)
-	
-	# pkldata = load_data_from_file(pklfile_path)
-	
-	# Call calculate in calculation/main_file.py
-	# and stage the file to send back to server
-	
-	# Send to server
-	
-finally:
-    sock.close()
-
-print "Sent:     {}".format(data)
-print "Received: {}".format(received)
-
+zfile_path = "./calculation.zip"
+pklfile_path = "./calculation/main_file.pkl"
 
 WORKER_UNAVAILABLE = 0
 WORKER_AVAILABLE = 1
@@ -59,6 +24,53 @@ def write_data_to_file(file_path, data):
 def load_data_from_file(file_path):
     with open(file_path, 'rb') as f:
         return pickle.load(f)
+
+# Create a socket (SOCK_STREAM means a TCP socket)                                       
+#sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+    # Connect to server and send data                                                    
+    #sock.connect((HOST, PORT))
+    #sock.sendall("Ready to do work")
+
+    # Receive data from the server and shut down                                         
+    #received = sock.recv(1024)
+	
+	# TODO Set vars to received information
+	# Save file to zfile_path with data received from sock
+	
+	# Unzip calculation.zip
+	zip_ref = zipfile.ZipFile(zfile_path, 'r')
+	zip_ref.extractall("./")
+	zip_ref.close()
+	
+	# TODO Create the file at pklfile_path from sock
+	write_data_to_file(pklfile_path, received)
+	
+	pkldata = load_data_from_file(pklfile_path)
+	
+	# Call calculate in calculation/main_file.py
+	# and stage the file to send back to server
+	from main_file import calculate
+	results = calculate(pkldata)
+	print results
+	# Send to server
+	# sock.sendall(results)
+	
+	# Save log as log.txt
+	# Check if log.txt exists and send to file
+	#if os.path.exists("./log.txt"):
+		# Send file data to server
+		#sock.sendall()
+	
+finally:
+	print "finally"
+#    sock.close()
+	# TODO Clean up files
+
+#print "Sent:     {}".format(data)
+#print "Received: {}".format(received)
+
 
 # Brody,
 #
